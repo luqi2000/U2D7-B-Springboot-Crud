5ed55f8f08b136a1039958cae901e451dbf8aa4a
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-07 20:40:03 +0100

    added new MainRunner with Fake random data

--- src/main/java/luqmanmohammad/U2D7BSpringbootCrud/UsersRunner.java
+++ src/main/java/luqmanmohammad/U2D7BSpringbootCrud/UsersRunner.java
@@ -1,14 +1,32 @@
 package luqmanmohammad.U2D7BSpringbootCrud;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import com.github.javafaker.Faker;
+
+import luqmanmohammad.U2D7BSpringbootCrud.users.User;
+import luqmanmohammad.U2D7BSpringbootCrud.users.UsersService;
 
 @Component
 public class UsersRunner implements CommandLineRunner{
-
+	@Autowired
+	UsersService usersService;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		Faker faker = new Faker(new Locale("it")); //ho creato un oggetto di tipo faker con date italiane
+		for(int i=0; i<20; i++) {
+			String name = faker.name().firstName();
+			String surname = faker.name().lastName();
+			String email = faker.internet().emailAddress();
+			
+			User user = new User(name,surname,email);
+			
+			usersService.create(user);
+		}
 		
 	}
 
