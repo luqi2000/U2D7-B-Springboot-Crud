815cb94e48f186b5291b6db4946b00f3604e25de
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-07 18:29:13 +0100

    Initial commit

---
+++ src/main/java/luqmanmohammad/U2D7BSpringbootCrud/U2D7BSpringbootCrudApplication.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D7BSpringbootCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2D7BSpringbootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2D7BSpringbootCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luqmanmohammad/U2D7BSpringbootCrud/U2D7BSpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D7BSpringbootCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2D7BSpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
